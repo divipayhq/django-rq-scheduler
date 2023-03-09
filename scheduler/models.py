b2ad65a9894b078639b95600b4907b83110f6fea
Russell Martin <russell@letsweel.com>
2023-03-09 15:18:35 +1100

    improvement: refactor django gettext_lazy for django 4 support

--- scheduler/models.py
+++ scheduler/models.py
@@ -8,7 +8,7 @@ from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.templatetags.tz import utc
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 import django_rq
 from model_utils import Choices
